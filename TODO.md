2a8608077ecde59994ea4ead946dffde3b0482c5
Matthew Weier O'Phinney <matthew@zend.com>
2012-10-15 11:28:26 -0500

    Run Markdown-generated HTML through HTMLPurifier
    - Added dep on HTMLPurifier
    - Updated highlight() view helper to sanitise markup generated by
      markdown

--- TODO.md
+++ TODO.md
@@ -8,7 +8,10 @@ X Basic paste functionality:
   X Populate form from existing paste
 - Second tier functionality:
   X Syntax highlighting
+    X HTML Purifier for markdown
+  - Captcha support
   - Split based on "##" sections
   - Embed paste via JS
+  - Move PhlyPaste\Mongo stuff into separate module
 - Third tier functionality:
   - Display themes
--- src/PhlyPaste/View/Highlight.php
+++ src/PhlyPaste/View/Highlight.php
@@ -3,6 +3,8 @@
 namespace PhlyPaste\View;
 
 use GeSHi;
+use HTMLPurifier;
+use HTMLPurifier_Config;
 use Zend\View\Helper\AbstractHelper;
 
 class Highlight extends AbstractHelper
@@ -10,13 +12,40 @@ class Highlight extends AbstractHelper
     public function __invoke($content, $language)
     {
         if ($language == 'markdown') {
-            $renderer = $this->getView();
-            $markdown = $renderer->plugin('markdown');
-            return $markdown($content);
+            return $this->renderMarkdown($content);
         }
 
         $geshi = new GeSHi($content, $language);
         $geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS, 5);
         return $geshi->parse_code();
+    }
+
+    /**
+     * Renders markdown
+     * 
+     * @param  string $content 
+     * @return string
+     */
+    protected function renderMarkdown($content)
+    {
+        $renderer  = $this->getView();
+        $markdown  = $renderer->plugin('markdown');
+        $dirtyHtml =  $markdown($content);
+        $cleanHtml = $this->sanitiseHtml($dirtyHtml);
+        return $cleanHtml;
+    }
+
+    /**
+     * Sanitises HTML by running it through HTMLPurifier using default 
+     * configuration.
+     * 
+     * @param  string $dirtyHtml 
+     * @return string
+     */
+    protected function sanitiseHtml($dirtyHtml)
+    {
+        $config   = HTMLPurifier_Config::createDefault();
+        $purifier = new HTMLPurifier($config);
+        return $purifier->purify($dirtyHtml);
     }
 }
