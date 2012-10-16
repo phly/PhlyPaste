457c57b1d61a7d0045fcbddc1cc10bd90eff79eb
Matthew Weier O'Phinney <matthew@zend.com>
2012-10-16 11:31:55 -0500

    Completed second push on API controller
    - Validate that we have an Accept header we can handle
    - Setup Json view strategy
    - Handle errors
    - Handle creation of pastes

--- src/PhlyPaste/Controller/ApiController.php
+++ src/PhlyPaste/Controller/ApiController.php
@@ -5,6 +5,7 @@ namespace PhlyPaste\Controller;
 use PhlyPaste\Model\Form as FormFactory;
 use PhlyPaste\Model\PasteServiceInterface;
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Mvc\MvcEvent;
 
 class ApiController extends AbstractActionController
 {
@@ -19,6 +20,28 @@ class ApiController extends AbstractActionController
     public function setPasteService(PasteServiceInterface $pastes)
     {
         $this->pastes = $pastes;
+    }
+
+    public function onDispatch(MvcEvent $e)
+    {
+        // Setup JsonStrategy
+        $services = $this->getServiceLocator();
+        $jsonStrategy = $services->get('ViewJsonStrategy');
+        $view         = $services->get('View');
+        $view->getEventManager()->attach($jsonStrategy, 100);
+
+        // Test that we have an appropriate "accept" header
+        $request = $e->getRequest();
+        $headers = $request->getHeaders();
+        $accept  = $headers->get('Accept');
+        if (false === $accept) {
+            return $this->createUnacceptableResponse($e);
+        }
+        if (!$accept->match('application/json')) {
+            return $this->createUnacceptableResponse($e);
+        }
+
+        return parent::onDispatch($e);
     }
 
     public function listAction()
@@ -79,12 +102,45 @@ class ApiController extends AbstractActionController
     public function createAction()
     {
         // get paste from raw body
+        $request = $this->getRequest();
+        $json    = $request->getContent();
+
         // decode paste from json as assoc array
+        $data = json_decode($json, true);
+
         // get form, and set paste data
+        $form = $this->formFactory->factory();
+        $form->setData($data);
+
         // if form invalid, report errors
+        if (!$form->isValid()) {
+            // create errors...
+            $model = $this->createError('invalid-paste');
+            $model->setVariable('errors', $form->getMessages());
+            return $model;
+        }
+
         // if form valid, attempt to create paste
-        // if exception, report error
+        $paste = $form->getData();
+        try {
+            $this->pastes->create($paste);
+        } catch (\Exception $e) {
+            // create errors...
+            return $this->createError('error-creating', $e->getMessage());
+        }
+
         // create payload to return
+        $canonical = $this->url('phly-paste/view', array('paste' => $paste->hash));
+        $response  = $e->getResponse();
+        $response->setStatusCode(201);
+
+        $response->getHeaders()->addHeaderLine('Location', $canonical);
+        return new JsonModel(array(
+            'links' => array(
+                $this->getLink('canonical', $canonical),
+                $this->getLink('self', $this->url('phly-paste/api/item', array('paste' => $paste->hash))),
+            ),
+        ));
     }
 
     protected function getPaste()
@@ -99,5 +155,59 @@ class ApiController extends AbstractActionController
             return;
         }
         return $paste;
+    }
+
+    protected function getLink($rel, $url, $page = null)
+    {
+        if ($page !== null) {
+            $url .= '?page=' . $page;
+        }
+        return array(
+            'rel'  => $rel,
+            'href' => $url,
+        );
+    }
+
+    protected function createUnacceptableResponse()
+    {
+        $response = $this->getResponse();
+        $response->setStatusCode(406);
+        $model = new JsonModel(array(
+            'error'   => 406,
+            'message' => 'Invalid Accept type; must be application/json to access the API',
+        ));
+        $e->setResult($model);
+        return $model;
+    }
+
+    protected function createError($error, $message = null)
+    {
+        $response = $this->getResponse();
+        $code     = 400;
+        switch ($error) {
+            case 'invalid-id':
+                $code    = 404;
+                $message = 'Paste not found';
+                break;
+            case 'error-creating':
+                $code = 500;
+                if (null === $message) {
+                    $message = 'Paste hash missing or invalid';
+                }
+                break;
+            case 'invalid-paste':
+            case 'missing-id':
+            default:
+                if (null === $message) {
+                    $message = 'Paste hash missing or invalid';
+                }
+                break;
+        }
+        $response->setStatusCode($code);
+        $model = new JsonModel(array(
+            'error'   => $code,
+            'message' => $message,
+        ));
+        return $model;
     }
 }
